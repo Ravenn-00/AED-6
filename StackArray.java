b08e5485df8e7321fdbb7186a38017f0ca54bb10
GRB <freze000@gmail.com>
2025-05-08 21:00:27 -0500

    Update StackArray.java

--- StackArray.java
+++ StackArray.java
@@ -6,13 +6,25 @@ public class StackArray<E> implements Stack<E> {
         tope = -1;
     }
     public void push(E x){
-    
+        if (isFull()) {
+            System.out.println("stack full");
+            return;
+        }
+        array[++tope] = x;    
     }
     public E pop() throws ExceptionIsEmpty {
-
+        if (isEmpty()) {
+            throw new ExceptionIsEmpty("stack vacio");
+        }
+        E element = array[tope];
+        array[tope--] = null;
+        return element;
     }
     public E top() throws ExceptionIsEmpty {
-
+        if (isEmpty()) {
+            throw new ExceptionIsEmpty("stack vacio");
+        }
+        return array[tope];
     }
     public boolean isEmpty() {
         return this.tope == -1;
@@ -29,7 +41,13 @@ public class StackArray<E> implements Stack<E> {
     
     }    
     public String toString(){
-    
+        String s = "Array: ";
+        for (E e : array) {
+            s += e + " ";
+            if(e == null)
+                return s;
+        }
+        return s;    
     }
 }
 
